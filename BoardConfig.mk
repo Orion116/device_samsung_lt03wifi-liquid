ebed838a4c00a28e090a4a7e3d6a2371f95845b5
Josh <joshwiley115@gmail.com>
2015-10-14 20:49:18 -0400

    This is a to maintain ownership

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ COMMON_GLOBAL_CFLAGS += -DSAMSUNG_DVFS
 # Kernel
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CONFIG := BrokenNote_lt03wifi_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_lt03wifi_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/lt03wifi
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 SM-P600 device repo for CyangenMod.
 
-# device_samsung_lt03wifi-liquid
